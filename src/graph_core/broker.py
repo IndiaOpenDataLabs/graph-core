657942f9978db4a211f636d9ee947ec064552757
Madhav Kandukuri <madhav165@gmail.com>
2026-05-31 21:25:29 +0530

    Enable async Dramatiq workers

--- src/graph_core/broker.py
+++ src/graph_core/broker.py
@@ -4,6 +4,8 @@ import os
 
 import dramatiq
 from dramatiq.brokers.redis import RedisBroker
+from dramatiq.middleware.asyncio import AsyncIO
 
 broker = RedisBroker(url=os.getenv("REDIS_URL", "redis://localhost:6379/0"))
+broker.add_middleware(AsyncIO())
 dramatiq.set_broker(broker)
